7aba7100c1c4110f75340063a8886eac4e05136b
daiyabarus <daiyabarus@gmail.com>
2023-11-27 13:19:59 +0700

    init.lua null-ls

--- lua/user/plugins/config/null_ls/init.lua
+++ lua/user/plugins/config/null_ls/init.lua
@@ -26,57 +26,32 @@ local sources = {
         },
     }),
     null_ls_sources.chktex,
-    -- Formatters
-    -- null_ls.builtins.formatting.prettierd,
-    -- null_ls.builtins.formatting.stylua.with({
-        --     extra_args = {
-            --         "--config-path",
-            --         vim.fn.expand("~/.stylua.toml"),
-            --     },
-            -- }),
+    Formatters
+    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.isort.with {
+        extra_args = {
+            "--fast",
+            "--profile",
+            "black" }
+        },
 
-            null_ls.builtins.formatting.isort.with {
-                extra_args = {
-                    "--fast",
-                    "--profile",
-                    "black" }
-                },
 
-                -- -- null_ls.builtins.formatting.ruff.with {
-                    -- --     extra_args = {
-                        -- --         "--config",
-                        -- --         vim.fn.expand("~/ruff.toml"),
-                        -- --     },
-                        -- -- },
 
-                        null_ls.builtins.formatting.black.with {
-                            extra_args = {
-                                "--fast", "--line-length", 80, "--experimental-string-processing"}
-                            },
+    null_ls.builtins.formatting.black.with {
+        extra_args = {
+            "--fast", "--line-length", 80, "--experimental-string-processing"}
+        },
 
-                            -- null_ls_sources.latexindent,
-                            -- -- Diagnostics
-                            -- -- null_ls.builtins.diagnostics.ruff.with({
-                                -- --     extra_args = {
-                                    -- --         "--config",
-                                    -- --         vim.fn.expand("~/ruff.toml"),
-                                    -- --     },
-                                    -- -- }),
+        null_ls_sources.latexindent,
 
-                                    -- null_ls.builtins.diagnostics.flake8.with({
-                                        --     extra_args = {
-                                            --         "--config",
-                                            --         vim.fn.expand("~/.flake8"),
-                                            --     },
-                                            -- }),
 
-                                            null_ls_sources.chktex,
-                                        }
+        null_ls_sources.chktex,
+    }
 
-                                        null_ls.setup({
-                                            debounce = 500,
-                                            default_timeout = 10000,
-                                            diagnostics_format = "#{m} (#{s})",
-                                            sources = sources,
-                                            on_attach = utils.on_attach,
-                                        })
+    null_ls.setup({
+        debounce = 500,
+        default_timeout = 10000,
+        diagnostics_format = "#{m} (#{s})",
+        sources = sources,
+        on_attach = utils.on_attach,
+    })
